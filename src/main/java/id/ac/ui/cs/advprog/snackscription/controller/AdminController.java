9ea98a60d54bb591520139519899fba61fa70399
sdikyarts <wtrcrss.family@gmail.com>
2024-04-17 20:58:22 +0700

    add model, repository, and services for subscription-admin

--- src/main/java/id/ac/ui/cs/advprog/snackscription/controller/AdminController.java
+++ src/main/java/id/ac/ui/cs/advprog/snackscription/controller/AdminController.java
@@ -1,12 +1,37 @@
 package id.ac.ui.cs.advprog.snackscription.controller;
 
+import id.ac.ui.cs.advprog.snackscription.model.AdminSubscription;
+import id.ac.ui.cs.advprog.snackscription.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private AdminService service;
+
     @GetMapping("")
-    public String adminPage() {return "admin";}
+    public String adminPage(Model model) {
+        AdminSubscription adminSubscription = new AdminSubscription();
+        model.addAttribute("adminSubscription", adminSubscription);
+        return "admin";
+    }
+
+    @PostMapping("/create")
+    public String createAdminSubscription(@ModelAttribute AdminSubscription adminSubscription, Model model) {
+        service.create(adminSubscription);
+        return "redirect:admin";
+    }
+
+    @GetMapping("/list")
+    public String subscriptionListPage(Model model){
+        List<AdminSubscription> adminSubscriptions = service.findAll();
+        model.addAttribute("adminSubscriptions", adminSubscriptions);
+        return "adminSubscriptionList";
+    }
 }
