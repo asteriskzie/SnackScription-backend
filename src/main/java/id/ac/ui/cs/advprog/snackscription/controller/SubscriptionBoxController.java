219a72753f0ca6eab1b9865650203c0f91650975
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-04-17 18:42:48 +0700

    add subscriptionbox controller skeleton dummy

--- src/main/java/id/ac/ui/cs/advprog/snackscription/controller/SubscriptionBoxController.java
+++ src/main/java/id/ac/ui/cs/advprog/snackscription/controller/SubscriptionBoxController.java
@@ -1,4 +1,15 @@
 package id.ac.ui.cs.advprog.snackscription.controller;
 
-public class SubscriptionBoxController  {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class SubscriptionBoxController {
+    @GetMapping("/Subscription-Box-Admin")
+    public String homePage() {
+        return "SubscriptionBoxAdmin";
+    }
 }
